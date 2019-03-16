623f3806bcbb9642449debb08009984ecaafdb98
wangzt <tomsky.wang@gmail.com>
2019-03-16 18:13:16 +0800

    add idea project

---
+++ idea/src/com/tomsky/MyApp.kt
@@ -0,0 +1,9 @@
+package com.tomsky
+
+import com.tomsky.linkedlist.LRUCache
+
+fun main(args: Array<String>) {
+    println("Hello kotlin")
+    var cache = LRUCache()
+    cache.input()
+}
---
+++ idea/src/com/tomsky/linkedlist/LRUCache.kt
@@ -0,0 +1,18 @@
+package com.tomsky.linkedlist
+
+class LRUCache {
+
+
+    fun input() {
+        println("I am lrucache")
+        var list = LinkedList<String>()
+        list.append("cache 1")
+        list.append("cache 2")
+
+        println(list.count())
+
+    }
+}
+
+
+ 
