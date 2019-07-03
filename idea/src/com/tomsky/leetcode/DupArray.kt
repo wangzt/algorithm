4c7feee1be4795b029fb3eea1603a6c9ca94b8a1
wangzhitao <wangzhitao@huajiao.tv>
2019-07-03 20:35:07 +0800

    per

--- idea/src/com/tomsky/leetcode/DupArray.kt
+++ idea/src/com/tomsky/leetcode/DupArray.kt
@@ -9,12 +9,17 @@ class DupArray {
 //            print("${input[i]} ")
 //        }
 
-        val input = intArrayOf(0,1,2,2,3,0,4,2)
-        val data = 2
+//        val input = intArrayOf(0,1,2,2,3,0,4,2)
+//        val data = 2
+//        val size = removeDupElement(input, data)
+//        println("size:$size")
+//        for (i in 0 until size) {
+//            print("${input[i]} ")
+//        }
 
-        val size = removeDupElement(input, data)
-        println("size:$size")
-        for (i in 0 until size) {
+        var input = intArrayOf(1,2,3)
+        nextPermutation(input)
+        for (i in 0 until input.size) {
             print("${input[i]} ")
         }
     }
@@ -48,5 +53,46 @@ class DupArray {
             }
         }
         return result
+    }
+
+    fun nextPermutation(nums: IntArray): Unit {
+        var localMin = -1
+        for (i in nums.size-1 downTo 1 ) {
+            if (nums[i] > nums[i-1]) {
+                localMin = i - 1
+                break
+            }
+        }
+
+        if (localMin == -1) {
+            swapSort(nums, 0, nums.size)
+            return
+        }
+
+        var nextHNumber = -1
+        var nextHIndex = -1
+        for (j in localMin+1 until nums.size) {
+            if (nums[j] > nums[localMin]) {
+                nextHNumber = nums[j]
+                nextHIndex = j
+            } else {
+                break
+            }
+        }
+        nums[nextHIndex] = nums[localMin]
+        nums[localMin] = nextHNumber
+        swapSort(nums, localMin+1, nums.size)
+    }
+
+    private fun swapSort(nums: IntArray, lo:Int, hi:Int) {
+        var i = lo
+        var j = hi - 1
+        while (i<j) {
+            var t = nums[j]
+            nums[j] = nums[i]
+            nums[i] = t
+            i++
+            j--
+        }
     }
 }
